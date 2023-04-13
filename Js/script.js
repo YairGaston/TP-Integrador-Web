1be7dd356546bea9ab71ca4d56854832f8b0c071
Yair <ygsanso@gmail.com>
2023-04-13 01:51:53 -0300

    Se agrega lógica al menú de hamburguesa del index y portfolios, se modifica JSON, se agrega como segunda solución al trabajo de las barras de navegación.

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,46 @@
+/* Acciones desencadenadas por el menu de hamburguesa y enlaces a páginas del sitio */
+/* ejemplo para agregar clase a un elemento nav.classList.add("enlacesMenu"); */
+
+const   btnMenu = document.querySelector("#btn-menu");
+const   btnContact = document.querySelector("#btn-contacto");
+const   btnNos = document.querySelector("#btn-nosotros");
+const   btnSolu = document.querySelector("#btn-soluciones");
+const   btnOfi = document.querySelector("#btn-oficinas");
+const   btnReu = document.querySelector("#btn-reunion");
+const   nav = document.querySelector("#enlaces");
+
+                btnMenu.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+                btnContact.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+                btnNos.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+                btnSolu.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+                btnOfi.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+                btnReu.addEventListener("click", () =>{
+                        nav.classList.toggle("enlacesVisibles");
+                })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* Esto funciona... falta que el nombre sea variable */
 
